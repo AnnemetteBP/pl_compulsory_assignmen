a159ffdf28c2d95d0e27f36778de8394553e5719
Annemette Pirchert <ampirchert@gmail.com>
2022-03-11 20:12:31 +0100

    Add project files.

---
+++ pl-compulsory-assignment/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pl_compulsory_assignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> times = new List<string>();
+        Stopwatch stopWatch = new Stopwatch();
+    
+        public MainWindow()
+        {
+            InitializeComponent();
+            resultList.Items.Clear();
+        }
+
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            stopWatch.Restart();
+            long start = long.Parse(startValue.Text);
+            long end = long.Parse(endValue.Text);
+            var orderedPrimesSeq = PrimeGenerator.GetPrimesSequential(start, end);
+            PrimeGenerator.measureTime(times, stopWatch);
+            resultList.Items.Add($"Number of primes: {orderedPrimesSeq.Count} in range {start} to {end}. \n The time for running this operation in sequential was: {stopWatch.Elapsed}");
+        }
+
+        private async void buttonClickTwo(object sender, RoutedEventArgs e)
+        {
+            long start = long.Parse(startValue.Text);
+            long end = long.Parse(endValue.Text);
+            List<long> orderedPrimesSeq = new List<long>();
+
+            await Task.Factory.StartNew(() => {
+                stopWatch.Restart();
+                var result = PrimeGenerator.GetPrimesParallel(start, end);
+                string theString = "";
+                bool stillWaiting = true;
+                while (stillWaiting)
+                {
+                    if (PrimeGenerator.tasks.All(t => t.IsCompleted))
+                    {
+                        PrimeGenerator.measureTime(times, stopWatch);
+                        stillWaiting = false;
+                    }
+                }
+                theString = $"Number of primes: {result.Count} in range {start} to {end}. \n The time for running this operation in parallel was: {stopWatch.Elapsed}";
+                return theString;
+            }, TaskCreationOptions.LongRunning).ContinueWith(t => {
+                resultList.Items.Add(t.Result);
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+    }
+}
---
+++ pl-compulsory-assignment/PrimeGenerator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pl_compulsory_assignment
+{
+    internal class PrimeGenerator
+    {
+        private static object objLock = new object();
+        public static List<Task> tasks = new List<Task>();
+        // Adding a stopwatch. 
+        public static void measureTime(List<string> times, Stopwatch stopWatch)
+        {
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            times.Add(String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10));
+        }
+
+        // Program checking if the datatype <long> is a prime number. 
+        public static bool IsPrime(long number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (long)Math.Floor(Math.Sqrt(number));
+
+            for (long i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        // Sequential Implementation. 
+        public static List<long> GetPrimesSequential(long first, long last)
+        {
+            List<long> result = new List<long>();
+
+            for (long i = first; i <= last; i++)
+            {
+                if (IsPrime(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+
+        // Parallel Implementation. 
+        public static List<long> GetPrimesParallel(long first, long last)
+        {
+            List<long> result = new List<long>();
+            int maxTasks = Environment.ProcessorCount;
+            long range = last - first;
+            long segmentSize = range / maxTasks + 1;
+
+            for (int i = 0; i < maxTasks; i++) {
+                Task task = Task.Run(() => {
+                    long start = first + (i * segmentSize) - (i == 0 ? 0 : 1);
+                    long end = first + ((i + 1) * segmentSize) - 2;
+                    var partition = GetPrimesSequential(start, end);
+                    lock (objLock)
+                    {
+                        result.AddRange(partition);
+                    }
+                });
+                tasks.Add(task);
+            }
+
+            return result;
+        }
+    }
+}
